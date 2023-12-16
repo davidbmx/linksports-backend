4176a6e7e800f9d1a014a853588813bc7b82f764
david chinchin <dmquito@hotmail.com>
2023-12-16 00:51:13 -0500

    feat: integrations

--- posts/models/post.py
+++ posts/models/post.py
@@ -6,12 +6,13 @@ from lib.models import MainModel
 
 # Create your models here.
 
+
 class Post(MainModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
-    videoUrl: models.TextField()
-    tags: models.TextField()
+    videoUrl = models.TextField()
+    tags = models.TextField()
     sport = models.CharField(max_length=100)
     num_likes = models.IntegerField(default=0)
     num_views = models.IntegerField(default=0)
--- posts/views.py
+++ posts/views.py
@@ -5,14 +5,20 @@ from .serializers import PostSerializer
 from .models import Post
 
 # /api/posts/
+
+
 class PostViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 # /api/mypost/
-class MyPostViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+
+
+class MyPostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticated]
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
